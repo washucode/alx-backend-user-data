d1d785192bcb712269a423d1527607ad0356a8ca
washucode <estherw8525@gmail.com>
2024-02-08 11:18:00 +0300

    correct pycodestyle errors

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,9 @@ obfuscated
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction:str,
-                  message:str, separator:str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Returns the log message obfuscated"""
     for field in fields:
         message = re.sub(rf'{field}=.*?{separator}',
