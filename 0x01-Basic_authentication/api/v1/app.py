acaa0a0f1df6474eb071aa6d11aa57bf2eb307e7
washucode <estherw8525@gmail.com>
2024-02-16 23:05:42 +0300

    task 6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,19 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth = os.getenv('AUTH_TYPE')
+if auth:
+    if auth == 'auth':
+        auth = Auth()
+    else:
+        auth = None
 
 
 @app.errorhandler(401)
@@ -33,6 +41,20 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.before_request
+def before_request() -> str:
+    """ Before request handler
+    """
+    if auth:
+        if auth.require_auth(request.path, ['/api/v1/status/',
+                                            '/api/v1/unauthorized/',
+                                            '/api/v1/forbidden/']):
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 if __name__ == "__main__":
