ccb24327907cc39bcfc2278db290c296dcd9d19d
washucode <estherw8525@gmail.com>
2024-02-22 11:00:18 +0300

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,7 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
-
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -23,6 +23,9 @@ if auth:
         auth = None
 elif auth_type == 'basic_auth':
     auth = BasicAuth()
+
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
 
 
 @app.errorhandler(401)
@@ -50,15 +53,19 @@ def not_found(error) -> str:
 def before_request() -> str:
     """ Before request handler
     """
-    if auth:
-        if auth.require_auth(request.path, ['/api/v1/status/',
-                                            '/api/v1/unauthorized/',
-                                            '/api/v1/forbidden/']):
+    if auth is None:
+        pass
+    else:
+        excluded_paths = ['/api/v1/status/',
+                          '/api/v1/unauthorized/',
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/']
+        setattr(request, 'current_user', auth.current_user(request))
+        if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
-            request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -32,6 +32,8 @@ def view_one_user(user_id: str = None) -> str:
         user = request.current_user
     if user is None:
         abort(404)
+    if request.current_user is None:
+        abort(404)
     return jsonify(user.to_json())
 
 
@@ -122,12 +124,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
-
-@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
-def view_me() -> str:
-    """ GET /api/v1/users/me
-    Return:
-      - User object JSON represented
-    """
-    return jsonify(request.current_user.to_json())
