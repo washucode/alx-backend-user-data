48d82e27afe4d8cb1189a02f49f64d0d3a7775cf
washucode <estherw8525@gmail.com>
2024-02-22 10:26:56 +0300

    task 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -20,6 +20,13 @@ class SessionAuth(Auth):
 
     user_id_by_session_id = {}
 
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """returns a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
     def create_session(self, user_id: str = None) -> str:
         """creates a session
         """
