0c787b0868ddac1a8f9ebe92a78290af4e433967
washucode <estherw8525@gmail.com>
2024-02-22 08:45:27 +0300

    check valid pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Returns a salted, hashed password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validates that the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
